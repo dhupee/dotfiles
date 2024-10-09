a996d7e07ebce520a4e72f14881ada52c3814530
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2024-10-10 00:37:38 +0700

    add helix in configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -8,6 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+            ./pkgs/helix.nix
     ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes"];
