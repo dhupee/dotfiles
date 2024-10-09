83b79733e5318079eff8c9936011fd3e87ced507
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2024-10-10 01:19:19 +0700

    adding comment on configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -49,8 +49,8 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  # services.xserver.displayManager.sddm.enable = true;
-  services.xserver.displayManager.sddm.wayland.enable = true;
+  # services.xserver.displayManager.sddm.enable = true; # this one uses x11
+  services.xserver.displayManager.sddm.wayland.enable = true; # this one uses wayland
   services.xserver.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
