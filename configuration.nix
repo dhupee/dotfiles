c1b267c8b7deb154390730608a38e68d069736d0
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2024-10-09 14:15:13 +0700

    add home-manager system wide

--- configuration.nix
+++ configuration.nix
@@ -102,9 +102,10 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
-      git
+        git
         wget
         curl
+        home-manager
   ];
 
   programs.neovim = {
