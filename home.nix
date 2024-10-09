202e5bd1643be21b3f3ef47b4e1274f0008d69dc
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2024-10-09 12:39:36 +0700

    fkin semicolon

--- home.nix
+++ home.nix
@@ -53,7 +53,7 @@
     #   org.gradle.daemon.idletimeout=3600000
     # '';
     
-    "~/.p10k.zsh".source = ./config/p10k.zsh
+    "~/.p10k.zsh".source = ./config/p10k.zsh;
   };
 
   # Home Manager can also manage your environment variables through
