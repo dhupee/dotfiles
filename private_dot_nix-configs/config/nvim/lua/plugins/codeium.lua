f6151fee16594d289efb14e4e6573326b16f5c7c
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2024-10-22 20:56:49 +0700

    nothing has worked just yet, LSP dont disturbed so no worry

--- private_dot_nix-configs/config/nvim/lua/plugins/codeium.lua
+++ private_dot_nix-configs/config/nvim/lua/plugins/codeium.lua
@@ -1,11 +1,23 @@
 return {
+  -- "Exafunction/codeium.nvim",
+  -- dependencies = {
+  --   "nvim-lua/plenary.nvim",
+  --   "hrsh7th/nvim-cmp",
+  -- },
+  -- opts = {
+  --   enable_chat = true,
+  --   wrapper = "$HOME/.scripts/srghma-codeium-nix-wrapper",
+  -- },
   "Exafunction/codeium.nvim",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "hrsh7th/nvim-cmp",
   },
-  opts = {
-    enable_chat = true,
-    wrapper = "~/.scripts/srghma-codeium-nix-wrapper",
-  },
+  config = function()
+    require("codeium").setup({
+      tools = {
+        language_server = "/nix/store/qbwpqkjc6a0szxjwdwmhkcvzwhrax3yk-codeium-1.20.9/bin/codeium_language_server",
+      },
+    })
+  end,
 }
