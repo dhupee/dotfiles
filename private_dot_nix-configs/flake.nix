604b9a2f377a3f1d6b9dc8c1734572e6067ff8fc
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2024-10-22 20:57:30 +0700

    update flake with plasma manager and nix-alien

--- private_dot_nix-configs/flake.nix
+++ private_dot_nix-configs/flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nix-alien.url = "github:thiagokokada/nix-alien";
 
     nix-on-droid = {
       url = "github:nix-community/nix-on-droid/release-24.05";
@@ -14,6 +15,12 @@
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    plasma-manager = {
+      url = "github:nix-community/plasma-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
   };
 
   outputs = {
@@ -22,17 +29,21 @@
     nixpkgs-unstable,
     nix-on-droid,
     home-manager,
+    nix-alien,
+    plasma-manager,
   }: let
     system = "x86_64-linux"; # Adjust this if you are using a different architecture
-    # pkgs = nixpkgs.legacyPackages.${system};
+
     pkgs = import nixpkgs {
       system = system;
       config.allowUnfree = true;
     };
+
     pkgs-unstable = import nixpkgs-unstable {
       system = system;
       config.allowUnfree = true;
     };
+
     lib = nixpkgs.lib;
   in {
     # Nix-on-Droid configuration
@@ -40,9 +51,6 @@
       default = nix-on-droid.lib.nixOnDroidConfiguration {
         pkgs = import nixpkgs {system = "aarch64-linux";};
         modules = [./droids/default.nix];
-        # extraSpecialArgs = {
-        #   pkgs-unstable = import nixpkgs-unstable { system = "aarch64-linux"; };
-        # };
       };
     };
 
@@ -50,9 +58,20 @@
     nixosConfigurations = {
       nitro = lib.nixosSystem {
         inherit system;
-        modules = [./desktop/nitro/configuration.nix];
+        modules = [
+          ./desktop/nitro/configuration.nix
+
+          ({self, ...}: let
+            nix-alien-pkgs = nix-alien.packages.${system};
+          in {
+            environment.systemPackages = with pkgs; [
+              nix-alien-pkgs.nix-alien
+            ];
+          })
+        ];
         specialArgs = {
           inherit pkgs-unstable;
+          inherit self;
         };
       };
       virts = lib.nixosSystem {
@@ -68,7 +87,11 @@
     homeConfigurations = {
       dhupee = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-        modules = [./home/dhupee.nix];
+        modules = [
+          ./home/dhupee.nix
+
+          plasma-manager.homeManagerModules.plasma-manager
+        ];
         extraSpecialArgs = {
           inherit pkgs-unstable;
         };
