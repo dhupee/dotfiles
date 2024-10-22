71e5aef2ded4293dbaace6a7259212f640b97978
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2024-10-22 11:34:05 +0700

    add nixpkgs unstable alongside the stable

--- private_dot_nix-configs/flake.nix
+++ private_dot_nix-configs/flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    # nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     nix-on-droid = {
       url = "github:nix-community/nix-on-droid/release-24.05";
@@ -16,11 +16,11 @@
     };
   };
 
-  outputs = { self, nixpkgs, nix-on-droid, home-manager }:
+  outputs = { self, nixpkgs, nixpkgs-unstable, nix-on-droid, home-manager }:
     let
       system = "x86_64-linux"; # Adjust this if you are using a different architecture
       pkgs = nixpkgs.legacyPackages.${system};
-      # pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
+      pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
       lib = nixpkgs.lib;
     in {
 
@@ -40,16 +40,16 @@
       nitro = lib.nixosSystem {
         inherit system;
         modules = [ ./desktop/nitro/configuration.nix ];
-        # extraSpecialArgs = {
-        #   inherit pkgs-unstable;
-        # };
+        extraSpecialArgs = {
+          inherit pkgs-unstable;
+        };
       };
       virts = lib.nixosSystem {
         inherit system;
         modules = [ ./desktop/virts/configuration.nix ];
-        # extraSpecialArgs = {
-        #   inherit pkgs-unstable;
-        #   };
+        extraSpecialArgs = {
+          inherit pkgs-unstable;
+          };
       };
     };
 
@@ -58,9 +58,9 @@
       dhupee = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
         modules = [ ./home/dhupee.nix ];
-        # extraSpecialArgs = {
-        #   inherit pkgs-unstable;
-        # };
+        extraSpecialArgs = {
+          inherit pkgs-unstable;
+        };
       };
     };
   };
