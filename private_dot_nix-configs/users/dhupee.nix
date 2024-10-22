0a7b5cea3f3a994e6b07f8ce8df00058a51e66a9
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2024-10-22 09:45:15 +0700

    add zsh as default shell

--- private_dot_nix-configs/users/dhupee.nix
+++ private_dot_nix-configs/users/dhupee.nix
@@ -5,10 +5,14 @@
     isNormalUser = true;
     description = "dhupee";
     extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.zsh;
     packages = with pkgs; [
       firefox
       kate
     #  thunderbird
     ];
   };
+
+  # just for the check dont get angry with me
+  programs.zsh.enable = true;
 }
