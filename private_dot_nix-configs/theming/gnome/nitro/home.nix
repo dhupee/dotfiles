1bee25c535273b87a2fd320a31b60f5b83dc1f8f
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2024-10-26 00:11:07 +0700

    open-bar unstable can be used

--- private_dot_nix-configs/theming/gnome/nitro/home.nix
+++ private_dot_nix-configs/theming/gnome/nitro/home.nix
@@ -1,11 +1,16 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  pkgs-unstable,
+  ...
+}: {
   programs = {
     gnome-shell = {
       enable = true;
       extensions = [
         {package = pkgs.gnomeExtensions.forge;}
         {package = pkgs.gnomeExtensions.user-themes;}
-        {package = pkgs.gnomeExtensions.open-bar;}
+        {package = pkgs-unstable.gnomeExtensions.open-bar;}
+        {package = pkgs.gnomeExtensions.dash-to-dock;}
       ];
     };
   };
