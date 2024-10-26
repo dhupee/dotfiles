0f7d37b0d39d32fa02e8a5a1dac61fd1dd97bb7d
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2024-10-27 01:39:06 +0700

    little reformat on input methods

--- private_dot_nix-configs/modules/systems/input-methods.nix
+++ private_dot_nix-configs/modules/systems/input-methods.nix
@@ -6,11 +6,13 @@
 }: {
   i18n.inputMethod = {
     enabled = "fcitx5";
-    fcitx5.addons = with pkgs; [
-      fcitx5-mozc
-      fcitx5-gtk
-      fcitx5-configtool
-    ];
-    fcitx5.waylandFrontend = true;
+    fcitx5 = {
+      addons = with pkgs; [
+        fcitx5-mozc
+        fcitx5-gtk
+        fcitx5-configtool
+      ];
+      waylandFrontend = true;
+    };
   };
 }
