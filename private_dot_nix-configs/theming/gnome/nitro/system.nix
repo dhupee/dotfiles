bdd5ab9862ad4947fb2146d0d788d94dc5babb01
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2024-10-26 18:29:55 +0700

    removing the bloat of gnome kekw

--- private_dot_nix-configs/theming/gnome/nitro/system.nix
+++ private_dot_nix-configs/theming/gnome/nitro/system.nix
@@ -1,8 +1,30 @@
-{...}: {
+{pkgs, ...}: {
   # Enable Gnome as Desktop Environment
   services.xserver = {
     enable = true;
     displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
   };
+
+  # I dont need any of this
+  environment.gnome.excludePackages =
+    (with pkgs; [
+      gnome-photos
+      gnome-tour
+      gedit # text editor
+    ])
+    ++ (with pkgs.gnome; [
+      # cheese # webcam tool
+      gnome-music
+      gnome-terminal
+      epiphany # web browser
+      geary # email reader
+      evince # document viewer
+      gnome-characters
+      totem # video player
+      tali # poker game
+      iagno # go game
+      hitori # sudoku game
+      atomix # puzzle game
+    ]);
 }
