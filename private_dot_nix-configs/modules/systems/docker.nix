355f2151ab8eadace7ecb99a39d39a69a89c1738
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2024-10-27 23:43:06 +0700

    add lazydocker and autoprune

--- private_dot_nix-configs/modules/systems/docker.nix
+++ private_dot_nix-configs/modules/systems/docker.nix
@@ -1,10 +1,17 @@
-{...}: {
+{pkgs, ...}: {
   virtualisation = {
     docker = {
       enable = true;
       rootless = {
         enable = true;
         setSocketVariable = true;
+      };
+      extraPackages = with pkgs; [
+        lazydocker
+      ];
+      autoPrune = {
+        enable = true;
+        dates = "weekly";
       };
     };
   };
