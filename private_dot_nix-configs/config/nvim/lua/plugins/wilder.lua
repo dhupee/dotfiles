8b6f2b07036a33f9fcd50f74fbfa60a74b5f4ecc
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2024-11-07 13:36:57 +0700

    manage to repair wilder

--- private_dot_nix-configs/config/nvim/lua/plugins/wilder.lua
+++ private_dot_nix-configs/config/nvim/lua/plugins/wilder.lua
@@ -7,8 +7,21 @@ return {
     wilder.setup({ modes = { ":", "/", "?" } })
 
     -- config
+    -- if there's failure in functions, run :UpdateRemotePlugins
     wilder.set_option("pipeline", {
-      wilder.branch(wilder.cmdline_pipeline(), wilder.search_pipeline()),
+      wilder.branch(
+        wilder.cmdline_pipeline({
+          -- sets the language to use, 'vim' and 'python' are supported
+          language = "vim",
+          -- 0 turns off fuzzy matching
+          -- 1 turns on fuzzy matching
+          -- 2 partial fuzzy matching (match does not have to begin with the same first letter)
+          fuzzy = 1,
+        }),
+        wilder.search_pipeline({
+          pattern = "fuzzy",
+        })
+      ),
     })
 
     -- rendering config
