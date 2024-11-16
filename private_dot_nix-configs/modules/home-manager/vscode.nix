376ca0a9d1ed6551874040c0dc7c023fce3cffa9
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2024-11-16 09:36:27 +0700

    add jsoncrack from vscode marketplace

--- private_dot_nix-configs/modules/home-manager/vscode.nix
+++ private_dot_nix-configs/modules/home-manager/vscode.nix
@@ -4,32 +4,42 @@
     package = pkgs.vscodium-fhs;
     enableUpdateCheck = false;
     enableExtensionUpdateCheck = false;
-    extensions = with pkgs.vscode-extensions; [
-      # tooling
-      aaron-bond.better-comments
-      golang.go
-      charliermarsh.ruff
-      editorconfig.editorconfig
-      foxundermoon.shell-format
-      hediet.vscode-drawio
-      jnoortheen.nix-ide
-      llvm-vs-code-extensions.vscode-clangd
-      mads-hartmann.bash-ide-vscode
-      ms-azuretools.vscode-docker
-      ms-python.debugpy
-      ms-python.python
-      ms-toolsai.jupyter
-      ms-toolsai.jupyter-keymap
-      ms-toolsai.jupyter-renderers
-      ms-toolsai.vscode-jupyter-cell-tags
-      ms-toolsai.vscode-jupyter-slideshow
-      oderwat.indent-rainbow
-      vscodevim.vim
-      yzhang.markdown-all-in-one
+    extensions = with pkgs.vscode-extensions;
+      [
+        # tooling
+        aaron-bond.better-comments
+        golang.go
+        charliermarsh.ruff
+        editorconfig.editorconfig
+        foxundermoon.shell-format
+        hediet.vscode-drawio
+        jnoortheen.nix-ide
+        llvm-vs-code-extensions.vscode-clangd
+        mads-hartmann.bash-ide-vscode
+        ms-azuretools.vscode-docker
+        ms-python.debugpy
+        ms-python.python
+        ms-toolsai.jupyter
+        ms-toolsai.jupyter-keymap
+        ms-toolsai.jupyter-renderers
+        ms-toolsai.vscode-jupyter-cell-tags
+        ms-toolsai.vscode-jupyter-slideshow
+        oderwat.indent-rainbow
+        vscodevim.vim
+        yzhang.markdown-all-in-one
 
-      # theming
-      dracula-theme.theme-dracula
-    ];
+        # theming
+        dracula-theme.theme-dracula
+        # ];
+      ]
+      ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "jsoncrack-vscode";
+          publisher = "AykutSarac";
+          version = "2.0.3";
+          sha256 = "0pa49xg63la6gz5q652h25i2mf7wlj90rch9mpprwnbj9rw4qrb2";
+        }
+      ];
     userSettings = {
       "workbench.colorTheme" = "Dracula";
       "files.autoSave" = "afterDelay";
