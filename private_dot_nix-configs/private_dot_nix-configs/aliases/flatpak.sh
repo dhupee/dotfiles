bdc975092f1a1b19242993bcce1d0b00fa4952db
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2025-01-28 13:47:00 +0700

    looks like i accidentaly copy stuffs

--- private_dot_nix-configs/private_dot_nix-configs/aliases/flatpak.sh
+++
@@ -1,9 +0,0 @@
-# Flatpak
-if [[ -d "$HOME/.var/app/org.openscad.OpenSCAD" ]]; then
-    alias openscad="flatpak run org.openscad.OpenSCAD"
-fi
-
-# Bottles CLI
-if [[ -d "$HOME/.var/app/com.usebottles.bottles" ]]; then
-    alias bottles-cli="flatpak run --command=bottles-cli com.usebottles.bottles"
-fi
