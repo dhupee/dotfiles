93d33ed302fc86e12993709668a8527bcf511adf
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2025-01-28 13:47:00 +0700

    looks like i accidentaly copy stuffs

--- private_dot_nix-configs/private_dot_nix-configs/modules/systems/cloudlare-warp.nix
+++
@@ -1,5 +0,0 @@
-{pkgs, ...}: {
-  environment.systemPackages = [pkgs.cloudflare-warp]; # for warp-svc
-  systemd.packages = [pkgs.cloudflare-warp]; # for warp-cli
-  systemd.targets.multi-user.wants = ["warp-svc.service"]; # causes warp-svc to be started automatically
-}
