459b6af650c673c95c5f731384f48bccedb83c94
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2025-03-05 16:20:19 +0700

    update helix configs for droids

--- private_dot_nix-configs/modules/home-manager/helix.nix
+++ private_dot_nix-configs/modules/home-manager/helix.nix
@@ -5,6 +5,27 @@
 }: {
   programs.helix = {
     enable = true;
+    settings = {
+      theme = "autumn_night_transparent";
+      editor.cursor-shape = {
+        normal = "block";
+        insert = "bar";
+        select = "underline";
+      };
+    };
+    languages.language = [
+      {
+        name = "nix";
+        auto-format = true;
+        formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
+      }
+    ];
+    themes = {
+      autumn_night_transparent = {
+        "inherits" = "autumn_night";
+        "ui.background" = {};
+      };
+    };
     extraPackages = with pkgs; [
       # C++
       clang-tools
