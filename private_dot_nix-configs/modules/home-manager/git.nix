85a5a9641ff6b5962270f865a7b8c49fc3030faf
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2025-03-10 16:00:23 +0700

    feat(Add commitizen-like custom command for lazygit):

--- private_dot_nix-configs/modules/home-manager/git.nix
+++ private_dot_nix-configs/modules/home-manager/git.nix
@@ -2,6 +2,7 @@
   # the issue starts to get annoying
   # so use traditional .gitconfig instead
   programs = {
+    # Git cli
     git = {
       enable = true;
       userEmail = "narutohaj00@gmail.com";
@@ -12,11 +13,9 @@
       delta = {
         enable = true;
       };
-      # extraConfig = {
-      #     user.name = "Daffa Haj Tsaqif";
-      #     user.email = "narutohaj00@gmail.com";
-      # };
     };
+
+    # Lazygit
     lazygit = {
       enable = true;
       settings = {
@@ -26,6 +25,110 @@
             "pager" = "delta --dark --paging=never --syntax-theme base16-256 -s";
           };
         };
+        customCommands = [
+          {
+            key = "C";
+            command = ''git commit -m "{{ .Form.Type }}{{if .Form.Scopes}}({{ .Form.Scopes }}){{end}}{{if eq .Form.Breaking `yes`}}!{{end}}: {{ .Form.Description }}"'';
+            description = "commit with commitizen";
+            context = "files";
+            prompts = [
+              {
+                type = "menu";
+                title = "Select the type of change you are committing.";
+                key = "Type";
+                options = [
+                  {
+                    name = "Feature";
+                    description = "A new feature";
+                    value = "feat";
+                  }
+                  {
+                    name = "Fix";
+                    description = "A bug fix";
+                    value = "fix";
+                  }
+                  {
+                    name = "Documentation";
+                    description = "Documentation only changes";
+                    value = "docs";
+                  }
+                  {
+                    name = "Styles";
+                    description = "Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)";
+                    value = "style";
+                  }
+                  {
+                    name = "Code Refactoring";
+                    description = "A code change that neither fixes a bug nor adds a feature";
+                    value = "refactor";
+                  }
+                  {
+                    name = "Performance Improvements";
+                    description = "A code change that improves performance";
+                    value = "perf";
+                  }
+                  {
+                    name = "Tests";
+                    description = "Adding missing tests or correcting existing tests";
+                    value = "test";
+                  }
+                  {
+                    name = "Builds";
+                    description = "Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)";
+                    value = "build";
+                  }
+                  {
+                    name = "Continuous Integration";
+                    description = "Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)";
+                    value = "ci";
+                  }
+                  {
+                    name = "Chores";
+                    description = "Other changes that don't modify src or test files";
+                    value = "chore";
+                  }
+                  {
+                    name = "Reverts";
+                    description = "Reverts a previous commit";
+                    value = "revert";
+                  }
+                ];
+              }
+              {
+                type = "input";
+                title = "Enter the scope(s) of this change.";
+                key = "Scopes";
+              }
+              {
+                type = "input";
+                title = "Enter the short description of the change.";
+                key = "Description";
+              }
+              {
+                type = "menu";
+                title = "Is this a breaking change?";
+                key = "Breaking";
+                options = [
+                  {
+                    name = "No";
+                    description = "This change does not introduce a breaking change.";
+                    value = "no";
+                  }
+                  {
+                    name = "Yes";
+                    description = "This change introduces a breaking change.";
+                    value = "yes";
+                  }
+                ];
+              }
+              {
+                type = "confirm";
+                title = "Is the commit message correct?";
+                body = "{{ .Form.Type }}{{if .Form.Scopes}}({{ .Form.Scopes }}){{end}}{{if eq .Form.Breaking `yes`}}!{{end}}: {{ .Form.Description }}";
+              }
+            ];
+          }
+        ];
       };
     };
   };
