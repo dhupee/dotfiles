e4353252c011a6d59b04f8c95f6c4fdf0090b282
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2025-03-17 09:08:04 +0700

    fix(clean unused aliases and add 'll' for droids):

--- private_dot_nix-configs/private_aliases/private_general.sh
+++ private_dot_nix-configs/private_aliases/private_general.sh
@@ -11,13 +11,10 @@ alias c="clear"
 # 	alias cat="bat"
 # fi
 
-# Tmux 256 colors
-# alias tmux='TERM=screen-256color tmux'
-
 # # Helix
 # alias hx="helix"
 hxf() {
-    hx $(fzf)
+	hx $(fzf)
 }
 
 # Copying Tmux.conf to tmate.conf
@@ -28,16 +25,6 @@ alias y="yazi"
 
 # Plasma
 alias plasma-restart="kquitapp5 plasmashell ; sleep 2s ; kstart5 plasmashell"
-
-# # Git
-# alias ga="git add"
-# alias gs="git status"
-# alias gb='git branch'
-# alias gl='git log'
-# alias gcm="git commit -m"
-# alias gco='git checkout'
-# alias gpsh="git push"
-# alias gpll="git pull"
 
 # Goenv
 alias goenv-update="cd ~/.goenv && git fetch --all && git pull && cd"
@@ -58,13 +45,14 @@ alias vm-stop="systemctl stop libvirtd"
 # LS & LL
 alias ls="ls --color='auto'"
 alias lsa="ls -a --color='auto'"
-alias lla="ll -a"
+alias ll="ls -l --color='auto'"
+alias lla="ls -la --color='auto'"
 
 # NVIM
 alias v="vim"
 alias nv="nvim"
 nvf() {
-    nvim $(fzf)
+	nvim $(fzf)
 }
 
 # Dotfile save
@@ -73,26 +61,11 @@ alias dotsave="sh $HOME/.scripts/dotsave.sh"
 # Symlink
 alias symlink="ln -s"
 
-# Flatpak aliases, tidy up
-
-# # Arch-based maintenance
-# alias orphanrm="bash $HOME/.scripts/remove-orphans.sh"
-# alias orphanrm-aur="yay -Yc"
-# alias cacherm="rm -rf ~/.cache/*"
-
 # Lazygit
 alias lg="lazygit"
 
 # Lazydocker
 alias lzd="lazydocker"
-
-# # Update Ohmyzsh's custom plugins
-# alias omz_custom_update="sh $HOME/.scripts/omz-custom-upgrade.sh"
-
-# zoxide as cd
-# if command -v zoxide >/dev/null; then
-# 	alias cd="z"
-# fi
 
 # Tunneling url/localhost
 alias tunnel="cloudflared tunnel --url $1"
@@ -107,16 +80,10 @@ alias bottles-backup="bash .scripts/bottles-backup.sh"
 alias fan-speed-full="sudo nbfc set --speed=100"
 alias fan-speed-auto="sudo nbfc set --auto"
 
-# # NVM
-# alias nvm-update="curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/$(curl -s https://api.github.com/repos/nvm-sh/nvm/releases/latest | grep '"tag_name"' | cut -d '"' -f 4)/install.sh" | bash"
-
-# FNM
-# alias fnm-update="curl -fsSL https://fnm.vercel.app/install | zsh -s -- --skip-shell"
-
 # Delete X number of latest shell history
 function delete_latest_history() {
-    # Default count is 1
-    local count=${1:-1}
-    echo "Deleting last $count shell history entries"
-    fc -p -1 | tail -n $count | awk "{print $1}" | xargs -I {} fc -d {}
+	# Default count is 1
+	local count=${1:-1}
+	echo "Deleting last $count shell history entries"
+	fc -p -1 | tail -n $count | awk "{print $1}" | xargs -I {} fc -d {}
 }
