6c333108a62045a798ba459daec11285630b5aef
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2025-03-19 22:28:35 +0700

    add KDE ricing, not sure if it works in fresh install though

--- private_dot_nix-configs/private_theming/plasma/nitro/system.nix
+++ private_dot_nix-configs/private_theming/plasma/nitro/system.nix
@@ -5,7 +5,10 @@
 }: {
   services.xserver.enable = true;
   # services.xserver.displayManager.sddm.enable = true; # this one uses x11
-  services.displayManager.sddm.wayland.enable = true; # this one uses wayland
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true; # this one uses wayland
+  };
   services.desktopManager.plasma6.enable = true;
 
   # environment.systemPackages = with pkgs; [
