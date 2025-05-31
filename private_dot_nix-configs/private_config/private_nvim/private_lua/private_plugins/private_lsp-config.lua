bc5a40b00c042fecca8e00001fc26cbf02717c06
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2025-05-31 10:25:43 +0700

    feat(gave up, also nvim lsp config tweak for nixd):

---
+++ private_dot_nix-configs/private_config/private_nvim/private_lua/private_plugins/private_lsp-config.lua
@@ -0,0 +1,94 @@
+-- Refer to this documentation:
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
+
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        bashls = {},
+        biome = {},
+        clangd = {},
+        dockerls = {},
+        gopls = {
+          settings = {
+            gopls = {
+              semanticTokens = true,
+              analyses = {
+                unusedparams = true,
+              },
+              staticcheck = true,
+              hints = {
+                assignVariableTypes = false,
+                compositeLiteralFields = false,
+                compositeLiteralTypes = false,
+                constantValues = false,
+                functionTypeParameters = false,
+                parameterNames = false,
+                rangeVariableTypes = false,
+              },
+            },
+          },
+        },
+        html = {},
+        jsonls = {},
+        lua_ls = {
+          settings = {
+            Lua = {
+              workspace = {
+                checkThirdParty = false,
+              },
+              completion = {
+                callSnippet = "Replace",
+              },
+            },
+          },
+        },
+        marksman = {},
+        nixd = {
+          nixpkgs = {
+            expr = "import <nixpkgs> { }",
+          },
+          formatting = {
+            command = { "nixfmt" },
+          },
+          options = {
+            nixos = {
+              expr = '(builtins.getFlake ("git+file://" + toString ./.)).nixosConfigurations.k-on.options',
+            },
+            home_manager = {
+              expr = '(builtins.getFlake ("git+file://" + toString ./.)).homeConfigurations."ruixi@k-on".options',
+            },
+          },
+        },
+        pyright = {
+          settings = {
+            python = {
+              analysis = {
+                autoSearchPaths = true,
+                useLibraryCodeForTypes = true,
+                typeCheckingMode = "basic",
+                diagnosticMode = "openFilesOnly",
+              },
+            },
+          },
+        },
+        ruff = {},
+        svelte = {},
+        tailwindcss = {},
+        terraformls = {},
+        texlab = {},
+        tflint = {},
+        ts_ls = {},
+        volar = {},
+        yamlls = {
+          settings = {
+            yaml = {
+              keyOrdering = false,
+            },
+          },
+        },
+      },
+    },
+  },
+}
