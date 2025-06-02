7708d07b91b04f5498d237beee32fd198901e680
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2025-06-02 08:59:14 +0700

    feat(update helix and nvim config, testing using it on phone):

--- private_dot_nix-configs/private_modules/private_home-manager/private_helix.nix
+++ private_dot_nix-configs/private_modules/private_home-manager/private_helix.nix
@@ -5,27 +5,27 @@
 }: {
   programs.helix = {
     enable = true;
-    settings = {
-      theme = "autumn_night_transparent";
-      editor.cursor-shape = {
-        normal = "block";
-        insert = "bar";
-        select = "underline";
-      };
-    };
-    languages.language = [
-      {
-        name = "nix";
-        auto-format = true;
-        formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
-      }
-    ];
-    themes = {
-      autumn_night_transparent = {
-        "inherits" = "autumn_night";
-        "ui.background" = {};
-      };
-    };
+    # settings = {
+    #   theme = "autumn_night_transparent";
+    #   editor.cursor-shape = {
+    #     normal = "block";
+    #     insert = "bar";
+    #     select = "underline";
+    #   };
+    # };
+    # languages.language = [
+    #   {
+    #     name = "nix";
+    #     auto-format = true;
+    #     formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
+    #   }
+    # ];
+    # themes = {
+    #   autumn_night_transparent = {
+    #     "inherits" = "autumn_night";
+    #     "ui.background" = {};
+    #   };
+    # };
     extraPackages = with pkgs; [
       # C++
       clang-tools
@@ -74,5 +74,12 @@
       # Yaml
       yaml-language-server
     ];
+
+    xdg.configFile = {
+      "helix" = {
+        source = ../../config/helix;
+        recursive = true;
+      };
+    };
   };
 }
