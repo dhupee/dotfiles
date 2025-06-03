fb73dac09df8ba0bcc4a39ccec455524b7e2eeb9
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2025-06-03 16:15:18 +0700

    feat(adding hop.nvim):

---
+++ private_dot_nix-configs/private_config/private_nvim/private_lua/private_plugins/hop.lua
@@ -0,0 +1,18 @@
+return {
+  "smoka7/hop.nvim",
+  version = "*",
+  opts = {
+    keys = "etovxqpdygfblzhckisuran",
+  },
+  config = function()
+    require("hop").setup()
+
+    -- Register the top-level <leader>h group with which-key
+    local wk = require("which-key")
+    wk.add({
+      { "<leader>h", group = "hop" }, -- group
+      { "<leader>hw", "<cmd>HopWord<cr>", desc = "Hop Word" },
+      { "<leader>hl", "<cmd>HopLine<cr>", desc = "Hop Line" },
+    })
+  end,
+}
