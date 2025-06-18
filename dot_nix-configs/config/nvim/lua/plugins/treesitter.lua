f714704d7592db2265b3e33e1f7e46882ff6240b
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2025-06-18 12:49:08 +0700

    feat(make tmpl readable as other files):

--- dot_nix-configs/config/nvim/lua/plugins/treesitter.lua
+++ dot_nix-configs/config/nvim/lua/plugins/treesitter.lua
@@ -1,41 +1,56 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    highlight = {
-      enable = true,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      highlight = {
+        enable = true,
+      },
+      rainbow = {
+        enable = true,
+        extended_mode = true,
+        max_file_lines = 1000,
+      },
+      indent = {
+        enable = true,
+      },
+      ensure_installed = {
+        "bash",
+        "c",
+        "cpp",
+        "csv",
+        "dockerfile",
+        "go",
+        "gomod",
+        "hcl",
+        "json",
+        "jsonc",
+        "lua",
+        "luadoc",
+        "luap",
+        "make",
+        "markdown",
+        "markdown_inline",
+        "nix",
+        "python",
+        "regex",
+        "terraform",
+        "vim",
+        "vimdoc",
+        "yaml",
+      },
     },
-    rainbow = {
-      enable = true,
-      extended_mode = true,
-      max_file_lines = 1000,
-    },
-    indent = {
-      enable = true,
-    },
-    ensure_installed = {
-      "bash",
-      "c",
-      "cpp",
-      "csv",
-      "dockerfile",
-      "go",
-      "gomod",
-      "hcl",
-      "json",
-      "jsonc",
-      "lua",
-      "luadoc",
-      "luap",
-      "make",
-      "markdown",
-      "markdown_inline",
-      "nix",
-      "python",
-      "regex",
-      "terraform",
-      "vim",
-      "vimdoc",
-      "yaml",
-    },
+  },
+  {
+    vim.filetype.add({
+      pattern = {
+        [".+%.toml%.tmpl"] = "toml",
+        [".+%.json%.tmpl"] = "json",
+        [".+%.yaml%.tmpl"] = "yaml",
+        [".+%.yml%.tmpl"] = "yaml",
+        -- add more mappings here, e.g.:
+        -- [".+%.lua%.tmpl"] = "lua",
+        -- [".+%.py%.tmpl"]  = "python",
+      },
+    }),
   },
 }
