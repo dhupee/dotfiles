05c069b2052d73b1753665cd711e190ff58d43ee
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2025-06-23 10:52:39 +0700

    feat(move this the scripts to bash subdir):

---
+++ dot_scripts/bash/tensorflow-gpu-check.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+python3 -c "import tensorflow as tf; print(tf.config.list_physical_devices('GPU'))"
+
+
--- dot_scripts/tensorflow-gpu-check.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-
-python3 -c "import tensorflow as tf; print(tf.config.list_physical_devices('GPU'))"
-
-
