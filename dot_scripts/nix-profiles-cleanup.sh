2c6254efbeeefc778ec5717499e3e77703ec4980
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2025-06-23 10:52:39 +0700

    feat(move this the scripts to bash subdir):

--- dot_scripts/nix-profiles-cleanup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-rm -rf ~/.local/state/nix/profiles/home-manager ~/.local/state/nix/profiles/home-manager-*-link ~/.local/state/nix/profiles/profile ~/.local/state/nix/profiles/profile-*-link
