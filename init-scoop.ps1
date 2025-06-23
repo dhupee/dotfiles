1774c57c8134cbdc6aac8e223626b4d8ebdaa929
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2025-06-23 14:51:50 +0700

    Edit init-scoop to give optional install path

--- init-scoop.ps1
+++ init-scoop.ps1
@@ -1,2 +1,14 @@
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+
+Invoke-RestMethod get.scoop.sh -OutFile 'install.ps1'
+
+# Uncomment and edit the following line to specify a custom Scoop install directory
+# $TARGET_PATH = 'D:\Applications\Scoop'
+
+if (-not $TARGET_PATH) {
+    Write-Host "No custom path set. Running default install."
+    powershell -ExecutionPolicy Bypass -File .\install.ps1
+} else {
+    Write-Host "Custom path detected: $TARGET_PATH"
+    powershell -ExecutionPolicy Bypass -File .\install.ps1 -ScoopDir $TARGET_PATH
+}
