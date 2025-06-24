149ccc15980de9b3a7b4c70e16b02ac444d60104
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2025-06-24 09:05:11 +0700

    feat(adjust zsh depcrecated shit):

--- dot_nix-configs/modules/home-manager/zoxide/nitro.nix
+++ dot_nix-configs/modules/home-manager/zoxide/nitro.nix
@@ -1,11 +1,12 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }: {
   programs = {
     zsh = {
-      initExtra = ''
+      initContent = lib.mkOrder 1000 ''
         export _ZO_DATA_DIR=$HOME/.local/share/chezmoi/mutable-configs/zoxide/nitro
 
         eval "$(zoxide init zsh)"
--- dot_nix-configs/modules/home-manager/zoxide/work.nix
+++ dot_nix-configs/modules/home-manager/zoxide/work.nix
@@ -1,12 +1,13 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }: {
   programs = {
     zsh = {
-      initExtra = ''
-        export _ZO_DATA_DIR=$HOME/.local/share/chezmoi/mutable-configs/zoxide/work
+      initContent = lib.mkOrder 1000 ''
+        export _ZO_DATA_DIR=$HOME/.local/share/chezmoi/mutable-configs/zoxide/work/
 
         eval "$(zoxide init zsh)"
       '';
