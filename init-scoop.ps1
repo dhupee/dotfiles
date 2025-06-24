97a0b1588681493058b69cc34d30b4018efc361e
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2025-06-24 15:47:24 +0700

    fix(make the file name more obvious):

--- init-scoop.ps1
+++ init-scoop.ps1
@@ -1,14 +1,14 @@
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
 
-Invoke-RestMethod get.scoop.sh -OutFile 'install.ps1'
+Invoke-RestMethod get.scoop.sh -OutFile 'install-scoop.ps1'
 
 # Uncomment and edit the following line to specify a custom Scoop install directory
 # $TARGET_PATH = 'D:\Applications\Scoop'
 
 if (-not $TARGET_PATH) {
     Write-Host "No custom path set. Running default install."
-    powershell -ExecutionPolicy Bypass -File .\install.ps1
+    powershell -ExecutionPolicy Bypass -File .\install-scoop.ps1
 } else {
     Write-Host "Custom path detected: $TARGET_PATH"
-    powershell -ExecutionPolicy Bypass -File .\install.ps1 -ScoopDir $TARGET_PATH
+    powershell -ExecutionPolicy Bypass -File .\install-scoop.ps1 -ScoopDir $TARGET_PATH
 }
