6002c559de913b6857ce58f840f8511795cf42cd
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2025-06-25 09:45:11 +0700

    feat(add change attribute directly to dotsave, old script not deleted):

--- dot_scripts/bash/dotsave.sh
+++ dot_scripts/bash/dotsave.sh
@@ -5,20 +5,20 @@
 rclone_remote="mega-dh"
 
 dir_array=(
-    "$HOME/.nix-configs/"
-    "$HOME/.scripts/"
-    "$HOME/.distrobox-recipe/"
-    "$HOME/.zellij-layouts/"
-    "$HOME/Templates/"
-    "$HOME/Wallpapers/"
+	"$HOME/.nix-configs/"
+	"$HOME/.scripts/"
+	"$HOME/.distrobox-recipe/"
+	"$HOME/.zellij-layouts/"
+	"$HOME/Templates/"
+	"$HOME/Wallpapers/"
 )
 
 # file_array=(
 # )
 
 encrypted_dir_array=(
-    "$HOME/.ssh/"
-    "$HOME/.secrets/"
+	"$HOME/.ssh/"
+	"$HOME/.secrets/"
 
 )
 
@@ -28,20 +28,22 @@ encrypted_dir_array=(
 #----------------------SAVING FILES AND DIRS----------------------
 
 for d in "${dir_array[@]}"; do
-    if [ -d $d ]; then
-        # echo "$d exists"
-        echo "adding $d"
-    else
-        echo "$d is not exists"
-        read -p "Would you like to make one? (y/n)?" choice
-        case "$choice" in
-        y | Y) echo "yes, creating directory $d" && mkdir $d ;;
-        n | N) echo "skipping" ;;
-        *) echo "choice is invalid" ;;
-        esac
+	if [ -d $d ]; then
+		# echo "$d exists"
+		echo "adding $d"
+	else
+		echo "$d is not exists"
+		read -p "Would you like to make one? (y/n)?" choice
+		case "$choice" in
+		y | Y) echo "yes, creating directory $d" && mkdir $d ;;
+		n | N) echo "skipping" ;;
+		*) echo "choice is invalid" ;;
+		esac
 
-    fi
-    chezmoi add $d
+	fi
+	# also change attribute with chezmoi
+	chezmoi add $d
+	chezmoi chattr --recursive -- -private $d
 done
 
 # for f in "${file_array[@]}"; do
@@ -58,20 +60,20 @@ done
 #-----------------ENCRYPTING AND ADDING DIRS AND FILES---------------------
 
 for d in "${encrypted_dir_array[@]}"; do
-    if [ -d $d ]; then
-        # echo "$d exists"
-        echo "adding and encrypting $d"
-    else
-        echo "$d is not exists"
-        read -p "Would you like to make one? (y/n)?" choice
-        case "$choice" in
-        y | Y) echo "yes, creating directory $d && mkdir $d" ;;
-        n | N) echo "skipping" ;;
-        *) echo "choice is invalid" ;;
-        esac
+	if [ -d $d ]; then
+		# echo "$d exists"
+		echo "adding and encrypting $d"
+	else
+		echo "$d is not exists"
+		read -p "Would you like to make one? (y/n)?" choice
+		case "$choice" in
+		y | Y) echo "yes, creating directory $d && mkdir $d" ;;
+		n | N) echo "skipping" ;;
+		*) echo "choice is invalid" ;;
+		esac
 
-    fi
-    chezmoi add --encrypt $d
+	fi
+	chezmoi add --encrypt $d
 done
 
 # for f in "${encrypted_file_array[@]}"; do
