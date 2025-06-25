d0657c3a839543063dfc87fc22e38094e172a8e2
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2025-06-25 15:48:39 +0700

    fix(that last slash cause the zoxide unhappy though not error):

--- dot_nix-configs/modules/home-manager/zoxide/work.nix
+++ dot_nix-configs/modules/home-manager/zoxide/work.nix
@@ -7,7 +7,7 @@
   programs = {
     zsh = {
       initContent = lib.mkOrder 1000 ''
-        export _ZO_DATA_DIR=$HOME/.local/share/chezmoi/mutable-configs/zoxide/work/
+        export _ZO_DATA_DIR=$HOME/.local/share/chezmoi/mutable-configs/zoxide/work
 
         eval "$(zoxide init zsh)"
       '';
