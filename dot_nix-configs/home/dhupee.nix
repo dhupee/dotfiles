46b4eab4b39f5a552ce7d783c15228c39cb8f327
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2025-07-23 23:41:01 +0700

    feat(switch back to gnome, KDE is too annoying for nix shit):

--- dot_nix-configs/home/dhupee.nix
+++ dot_nix-configs/home/dhupee.nix
@@ -26,8 +26,8 @@
     ../modules/home-manager/spicetify.nix
 
     # theming
-    # ../theming/gnome/nitro/home.nix
-    ../theming/plasma/dracula/home.nix
+    ../theming/gnome/nitro/home.nix
+    # ../theming/plasma/dracula/home.nix
   ];
 
   # Home Manager needs a bit of information about you and the paths it should
