97fc5ecdc234856871969ab6a6231ca4e5d5cc72
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2025-08-22 21:13:43 +0700

    feat(add those second SSD):

--- dot_nix-configs/linux/nitro/configuration.nix
+++ dot_nix-configs/linux/nitro/configuration.nix
@@ -151,6 +151,18 @@
   programs.nix-ld.enable = true;
   programs.nix-ld.libraries = [];
 
+  # File Systems, adding secondary SSD
+  fileSystems."/mnt/secondDrive" = {
+    device = "/dev/disk/by-uuid/1b339a8a-3c99-4c16-9e93-976364b6698e";
+    fsType = "ext4";
+    options = [
+      # If you don't have this options attribute, it'll default to "defaults"
+      # boot options for fstab. Search up fstab mount options you can use
+      "users" # Allows any user to mount and unmount
+      "nofail" # Prevent system from failing if this drive doesn't mount
+    ];
+  };
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
