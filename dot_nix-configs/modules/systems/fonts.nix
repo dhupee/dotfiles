aa20018923ce72b5a78368c5a4597c5b62a9c04a
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2025-08-30 10:14:50 +0700

    feat(clean up, adjust to 25.05):

--- dot_nix-configs/modules/systems/fonts.nix
+++ dot_nix-configs/modules/systems/fonts.nix
@@ -1,20 +1,17 @@
 {pkgs, ...}: {
   fonts = {
     enableDefaultPackages = true;
-    packages = with pkgs; [
-      hachimarupop
-      noto-fonts-cjk-sans
-      corefonts
-
-      # now there's easier way that override
-      # not removing it yet
-      nerd-fonts.fira-code
-      nerd-fonts.hack
-
-      # I dont need all of them
+    packages =
+      (with pkgs; [
+        hachimarupop
+        noto-fonts-cjk-sans
+        corefonts
+      ])
       # Refer to this: https://github.com/ryanoasis/nerd-fonts?tab=readme-ov-file#patched-fonts
-      # (pkgs.nerd-fonts.override {fonts = ["fira-code" "hack"];})
-    ];
+      ++ (with pkgs.nerd-fonts; [
+        fira-code
+        hack
+      ]);
 
     fontconfig = {
       defaultFonts = {
