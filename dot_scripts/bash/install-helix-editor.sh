6cd34226f139811e59198fd61871c09a6c0c8bcb
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2025-10-28 15:03:16 +0700

    style(shfmt kicked in):

--- dot_scripts/bash/install-helix-editor.sh
+++ dot_scripts/bash/install-helix-editor.sh
@@ -12,7 +12,7 @@ wget -P "$DOWNLOAD_PATH" "https://github.com/helix-editor/helix/releases/downloa
 
 # if the path doesnt exist simply make it
 if [ ! -d $EXTRACT_DIR ]; then
-    mkdir $EXTRACT_DIR
+	mkdir $EXTRACT_DIR
 fi
 
 # Extracting helix
@@ -20,12 +20,12 @@ tar -xvf "${DOWNLOAD_PATH}helix-${VERSION}-${ARCH}.tar.xz" -C "${EXTRACT_DIR}"
 
 # create config directory
 if [ ! -d $CONFIG_DIR ]; then
-    mkdir $CONFIG_DIR
+	mkdir $CONFIG_DIR
 fi
 
 # create config directory
 if [ ! -d $BIN_DIR ]; then
-    mkdir $BIN_DIR
+	mkdir $BIN_DIR
 fi
 
 # copy things
