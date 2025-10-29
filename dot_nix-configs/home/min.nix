60b02f2a64fd4bc1c8e119d6cbd5b9c33319524d
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2025-10-29 10:19:52 +0700

    feat(add min home-manager profile):
    feat(accidentally deleted pkgs-unstable):

---
+++ dot_nix-configs/home/min.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  config,
+  inputs,
+  pkgs,
+  pkgs-unstable,
+  ...
+}: {
+  imports = [
+    # tooling
+    ../modules/home-manager/neovim.nix
+    ../modules/home-manager/git.nix
+    ../modules/home-manager/gh.nix
+    ../modules/home-manager/starship/desktop.nix
+    ../modules/home-manager/tmux.nix
+    ../modules/home-manager/yazi.nix
+    ../modules/home-manager/zsh.nix
+  ];
+
+  # Don't change this without reading the docs.
+  home.stateVersion = "24.05";
+
+  nixpkgs.config.allowUnfree = true;
+  nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
+
+  home.packages = with pkgs; [
+    bat
+    chezmoi
+    dust
+    fzf
+    go-task
+    htop
+    platformio-core
+    tectonic
+    thefuck
+    tldr
+    tree
+  ];
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
