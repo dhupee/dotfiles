eff0ed04bee554eedff0c08284fbda958a2a2a01
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2025-10-29 08:24:33 +0700

    feat(extract the path as variable):

--- dot_nix-configs/aliases/flatpak.sh
+++ dot_nix-configs/aliases/flatpak.sh
@@ -1,9 +1,11 @@
 # Flatpak
-if [[ -d "$HOME/.var/app/org.openscad.OpenSCAD" ]]; then
-	alias openscad="flatpak run org.openscad.OpenSCAD"
+OPENSCAD_PATH="$HOME/.var/app/org.openscad.OpenSCAD"
+if [[ -d $OPENSCAD_PATH ]]; then
+    alias openscad="flatpak run org.openscad.OpenSCAD"
 fi
 
 # Bottles CLI
-if [[ -d "$HOME/.var/app/com.usebottles.bottles" ]]; then
-	alias bottles-cli="flatpak run --command=bottles-cli com.usebottles.bottles"
+BOTTLES_PATH="$HOME/.var/app/com.usebottles.bottles"
+if [[ -d $BOTTLES_PATH ]]; then
+    alias bottles-cli="flatpak run --command=bottles-cli com.usebottles.bottles"
 fi
