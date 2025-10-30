7318a8fc6c863d6cd975e4e3f051294058cde729
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2025-10-30 13:55:57 +0700

    feat(add flutter-tools.lua and lazy-lock update):

---
+++ dot_nix-configs/config/nvim/lua/plugins/flutter-tools.lua
@@ -0,0 +1,11 @@
+return {
+  "nvim-flutter/flutter-tools.nvim",
+  lazy = false,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "stevearc/dressing.nvim", -- optional for vim.ui.select
+  },
+  config = function()
+    require("flutter-tools").setup({}) -- use defaults
+  end,
+}
