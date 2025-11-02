aa502026b55cc2d9c46e5912bdfa0dc757d4e349
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2025-11-02 19:43:56 +0700

    feat(add horrible extensions for a gag):

--- dot_nix-configs/modules/home-manager/spicetify.nix
+++ dot_nix-configs/modules/home-manager/spicetify.nix
@@ -9,9 +9,11 @@ in {
   programs.spicetify = {
     enable = true;
     # spicetifyPackage = pkgs.spicetify-cli;
+    # spotifyPackage = pkgs.spotify;
     enabledExtensions = with spicePkgs.extensions; [
       adblock
       hidePodcasts
+      catJamSynced
       shuffle # shuffle+ (special characters are sanitized out of extension names)
     ];
     enabledCustomApps = with spicePkgs.apps; [
@@ -19,6 +21,20 @@ in {
       marketplace
       newReleases
       historyInSidebar
+      ncsVisualizer
+      # {
+      #   # The source of the customApp
+      #   # make sure you're using the correct branch
+      #   # It could also be a sub-directory of the repo
+      #   src = pkgs.fetchFromGitHub {
+      #     owner = "";
+      #     repo = "";
+      #     rev = "";
+      #     hash = "";
+      #   };
+      #   # The actual file name of the customApp usually ends with .js
+      #   name = "";
+      # }
     ];
     theme = spicePkgs.themes.dracula;
     # colorScheme = "macchiato";
