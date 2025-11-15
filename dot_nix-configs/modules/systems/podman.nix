e816516301c37c60d155416582c20ba09881f0bb
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2025-11-15 07:55:23 +0700

    feat(use both docker and podman, podman only for distrobox):
    forgot

--- dot_nix-configs/modules/systems/podman.nix
+++ dot_nix-configs/modules/systems/podman.nix
@@ -6,10 +6,10 @@
       enable = true;
       dates = "weekly";
     };
-    dockerCompat = true;
-    dockerSocket = {
-      enable = true;
-    };
-    defaultNetwork.settings.dns_enabled = true; # Required for containers under podman-compose to be able to talk to each other.
+    # dockerCompat = true;
+    # dockerSocket = {
+    #   enable = true;
+    # };
+    # defaultNetwork.settings.dns_enabled = true; # Required for containers under podman-compose to be able to talk to each other.
   };
 }
