71d38864cc8d0779c574960c73eadbdb2da8289c
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2025-12-03 07:36:43 +0700

    feat(can use lazydocker, not sure where i got it wrong the last time):

--- dot_nix-configs/modules/systems/podman.nix
+++ dot_nix-configs/modules/systems/podman.nix
@@ -1,18 +1,22 @@
 {pkgs, ...}: {
-  virtualisation.podman = {
-    enable = true;
-    package = pkgs.podman;
-    autoPrune = {
+  virtualisation = {
+    containers.enable = true;
+    podman = {
       enable = true;
-      dates = "weekly";
+      package = pkgs.podman;
+      autoPrune = {
+        enable = true;
+        dates = "weekly";
+      };
+      extraPackages = with pkgs; [
+        docker-compose
+        podman-compose
+      ];
+      dockerCompat = true;
+      dockerSocket = {
+        enable = true;
+      };
+      defaultNetwork.settings.dns_enabled = true; # Required for containers under podman-compose to be able to talk to each other.
     };
-    extraPackages = with pkgs; [
-      podman-compose
-    ];
-    # dockerCompat = true;
-    # dockerSocket = {
-    #   enable = true;
-    # };
-    defaultNetwork.settings.dns_enabled = true; # Required for containers under podman-compose to be able to talk to each other.
   };
 }
