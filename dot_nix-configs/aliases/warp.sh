22db838cdcf2149c4032b27de26c7ce00c97585a
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2026-06-03 13:23:46 +0700

    fix(removing unused shell aliases):

--- dot_nix-configs/aliases/warp.sh
+++ dot_nix-configs/aliases/warp.sh
@@ -1,11 +1,5 @@
 # Aliases for WARP-CLI 1.1.1.1
 
-# # Start WARP Services
-# alias warp-service-start="systemctl start warp-svc.service"
-#
-# # Stops WARP Services
-# alias warp-service-stop="systemctl stop warp-svc.service"
-
 # Initialize the app
 alias warp-init="warp-cli registration new ; sleep 1s ; warp-cli mode warp+doh"
 
