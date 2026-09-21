8153292e238016a4b6e063ba8da5ecfa50354b37
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2026-09-22 05:53:20 +0700

    feat(nitro config): add custom udev for Xilinx Vivado
    fix
    
    refactor

--- dot_nix-configs/linux/nitro/configuration.nix
+++ dot_nix-configs/linux/nitro/configuration.nix
@@ -4,7 +4,32 @@
   pkgs,
   pkgs-unstable,
   ...
-}: {
+}: let
+  # Custom Udev file for enabling programming FPGA in Xilinx Vivado
+  custom-vivado-rules =
+    pkgs.writeTextFile
+    {
+      name = "10-xilinx-vivado.rules";
+      destination = "/etc/udev/rules.d/10-xilinx-vivado.rules";
+      text = ''
+        # xilinx-ftdi-usb.rules
+        ACTION=="add", ATTR{idVendor}=="0403", ATTR{manufacturer}=="Xilinx", MODE:="666"
+
+        # xilinx-digilent-usb.rules
+        ATTR{idVendor}=="1443", MODE:="666"
+        ACTION=="add", ATTR{idVendor}=="0403", ATTR{manufacturer}=="Digilent", MODE:="666"
+
+        # xilinx-pcusb.rules
+        ATTR{idVendor}=="03fd", ATTR{idProduct}=="0008", MODE="666"
+        ATTR{idVendor}=="03fd", ATTR{idProduct}=="0007", MODE="666"
+        ATTR{idVendor}=="03fd", ATTR{idProduct}=="0009", MODE="666"
+        ATTR{idVendor}=="03fd", ATTR{idProduct}=="000d", MODE="666"
+        ATTR{idVendor}=="03fd", ATTR{idProduct}=="000f", MODE="666"
+        ATTR{idVendor}=="03fd", ATTR{idProduct}=="0013", MODE="666"
+        ATTR{idVendor}=="03fd", ATTR{idProduct}=="0015", MODE="666"
+      '';
+    };
+in {
   imports = [
     # Hardware
     ../../machines/nitro/hardware-configuration.nix
@@ -171,11 +196,18 @@
   # List services that you want to enable:
   services.openssh.enable = true;
 
-  # Make sure udev is installed for platformio
-  services.udev.packages = with pkgs; [
-    platformio-core.udev
-    stlink
-  ];
+  # Udev for embedded works
+  services.udev = {
+    packages =
+      (with pkgs; [
+        platformio-core.udev
+        stlink
+      ])
+      ++ [
+        # Check the let-in above
+        custom-vivado-rules
+      ];
+  };
 
   # adding /bin/bash, needed by platformio
   system.activationScripts.binbash = ''
