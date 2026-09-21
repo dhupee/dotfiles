a3d791c2191d404aae75b2876f4fead74bb57b2e
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2026-09-21 19:35:28 +0700

    docs(qemu host): removing unused settings

--- dot_nix-configs/modules/systems/qemu-host.nix
+++ dot_nix-configs/modules/systems/qemu-host.nix
@@ -25,8 +25,4 @@
     dnsmasq
   ];
   boot.binfmt.emulatedSystems = ["aarch64-linux"];
-
-  # Use this when using NixOS as Guest
-  # services.qemuGuest.enable = true;
-  # services.spice-vdagentd.enable = true;  # enable copy and paste between host and guest
 }
